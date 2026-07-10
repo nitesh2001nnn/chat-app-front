12e3ad3c75b5e6d0608cbbfe21a6f8d8dae01c16
nitesh nimje <niteshnimje5@gmail.com>
2026-07-11 01:25:48 +0530

    profil pge started

---
+++ src/page/profile-page/profile-constant/profile-constant.ts
@@ -0,0 +1,14 @@
+export const profileData = [
+    {
+        id: 1,
+        label: "Profile",
+        icon: "/assets/icons/logo.png",
+        subLabels: ["Name", "Profile Picture", "Username"]
+    },
+    {
+        id: 2,
+        label: "Chats",
+        icon: "/assets/icons/chat.png",
+        subLabels: ["Theme", "Wallpaper", "Chat Settings"]
+    }
+]
--- src/page/profile-page/profile-page.tsx
+++ src/page/profile-page/profile-page.tsx
@@ -1,10 +1,38 @@
+import Avatar from "../../common-component/avatar/avatar";
+import { profileData } from "./profile-constant/profile-constant";
 import "./profile-page.scss";
 
 const ProfilePage = () => {
   return (
     <div className="profile-page-container">
-        <div>Nitesh Nimje</div>
-      <span>Profile page</span>
+      <div className="main-text bold-text-medium">Nitesh Nimje</div>
+
+      <div className="avatar-place">
+        <div className="avatar-center">
+          <Avatar />
+        </div>
+      </div>
+      {profileData.map((itx: any, index: number) => {
+        return (
+          <div className="profile-data-container" key={index}>
+            <img src={itx.icon} />
+            <div className="side-label-container">
+              <div className="bold-text-medium-xxs">{itx.label}</div>
+              <div className="lower-container">
+                {itx?.subLabels?.map((item: string, ind: number) => {
+                  return (
+                    <span className="text-body-xs lower-text">{item}, </span>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        );
+      })}
+      <div className="logout-container">
+        <img src="/assets/icons/logout-24.png" />
+        <span className="bold-text-medium-xs">Logout</span>
+      </div>
     </div>
   );
 };
